9cae27dc2f345160a0252d769bce3f408831c8df
katerynarieznik <rieznik.k@gmail.com>
2023-09-23 12:50:53 +0300

    add and style nav

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<script>
+	import '../styles.css';
+</script>
+
+<nav>
+	<a href="/">SvelteFlix</a>
+
+	<div class="links">
+		<a href="/search">Search</a>
+		<a href="/watchlist">Watchlist</a>
+		<a href="/login">Log in</a>
+	</div>
+</nav>
+
+<style>
+	nav {
+		display: flex;
+		width: 100%;
+		height: 3rem;
+		align-items: center;
+		justify-content: space-between;
+		max-width: var(--column);
+		padding: 0 var(--side);
+		color: var(--accent);
+		margin: 0 auto;
+	}
+
+	a {
+		color: inherit;
+		text-decoration: none;
+	}
+</style>
