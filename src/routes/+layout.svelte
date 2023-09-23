eeafb44fb2557cb24d8fb83f7eb3f92a3cedb599
katerynarieznik <rieznik.k@gmail.com>
2023-09-23 13:05:48 +0300

    add logos and footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
 	import '../styles.css';
+	import logo from '$lib/images/logo.svg';
+	import tmdb from '$lib/images/tmdb.svg';
 </script>
 
 <nav>
-	<a href="/">SvelteFlix</a>
+	<a href="/"><img alt="SvelteFlix" src={logo} /></a>
 
 	<div class="links">
 		<a href="/search">Search</a>
@@ -11,6 +13,14 @@
 		<a href="/login">Log in</a>
 	</div>
 </nav>
+
+<main>
+	<slot />
+</main>
+
+<footer>
+	Data provided by <a href="https://www.themoviedb.org"><img alt="The Movie DB" src={tmdb} /></a>
+</footer>
 
 <style>
 	nav {
@@ -28,5 +38,20 @@
 	a {
 		color: inherit;
 		text-decoration: none;
+	}
+
+	img {
+		height: 1rem;
+	}
+
+	.links {
+		display: flex;
+		gap: 1rem;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 	}
 </style>
